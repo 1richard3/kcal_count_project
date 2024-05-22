7ab0808a7508a8da721a615e618ffd3e3e7a84e3
yyxmeng <Aries02090001@gmail.com>
2024-05-22 18:20:58 +0800

    Add files via upload
    新增由phpmyadmin製作的資料庫sql檔漢醫些基礎的stored procedure、新增用於連線phpmyadmin資料庫的js檔

---
+++ DBConnection.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: "csfood"
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+})  
+
+//以上是連線資料庫
+//以下是測試抓取資料
+
+$query = "SELECT * FROM comm";
+
+connection.query($query, function(err, rows, fields){
+    if(err){
+        console.log("An error occured with the query");
+        return;
+    }
+
+    console.log("Query successfully executed", rows);
+})
+
+connection.end(function(){
+    console.log("Connection closed");
+})  
