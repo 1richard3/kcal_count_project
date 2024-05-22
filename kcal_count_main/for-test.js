05e9784c60c37a527b9ba7d0cb4cfc472e142a82
1richard3 <richard921113richard@gmail.com>
2024-05-23 05:30:34 +0800

    做出固定sidebar 食物卡

--- kcal_count_main/for-test.js
+++ kcal_count_main/for-test.js
@@ -1,5 +1,70 @@
-var fruits = ['Apple', 'Banana'];
+// 以下是全家的資料陣列------------------------------------------------------------------------------------------------------------------------------
+const xlsx = require('xlsx');
+const path = require('path');
 
-// 輸出 2
-console.log(fruits.length)
+// 讀取 Excel 文件
+const familymartfilePath = path.resolve('../product_data/Family_product.xlsx');
+const familymartworkbook = xlsx.readFile(familymartfilePath);
 
+// 選擇要讀取的工作表
+const familymartsheetName = familymartworkbook.SheetNames[0]; // 讀取第一個工作表
+const familymartworksheet = familymartworkbook.Sheets[familymartsheetName];
+
+// 將工作表轉換為 JSON 格式
+const familymartdata = xlsx.utils.sheet_to_json(familymartworksheet, { header: 1 });
+
+const familymartarray = familymartdata.slice(1).map(item => ({
+    "商店": item[0],
+    "品名": item[1],
+    "價格": item[2],
+    "熱量": item[3]
+  }));
+// 以上是全家的資料陣列------------------------------------------------------------------------------------------------------------------------------
+
+console.log('全家的資料陣列: ', familymartarray);
+
+// 以下是小七的資料陣列------------------------------------------------------------------------------------------------------------------------------
+
+// 讀取 Excel 文件
+const sevenfilePath = path.resolve('../product_data/711_product.xlsx');
+const sevenworkbook = xlsx.readFile(sevenfilePath);
+
+// 選擇要讀取的工作表
+const sevensheetName = sevenworkbook.SheetNames[0]; // 讀取第一個工作表
+const sevenworksheet = sevenworkbook.Sheets[sevensheetName];
+
+// 將工作表轉換為 JSON 格式
+const sevendata = xlsx.utils.sheet_to_json(sevenworksheet, { header: 1 });
+
+const sevenarray = sevendata.slice(1).map(item => ({
+    "商店": item[0],
+    "品名": item[1],
+    "價格": item[2],
+    "熱量": item[3]
+  }));
+// 以上是小七的資料陣列------------------------------------------------------------------------------------------------------------------------------
+
+console.log('小七的資料陣列: ', sevenarray);
+
+// 以下是ok的資料陣列------------------------------------------------------------------------------------------------------------------------------
+
+// 讀取 Excel 文件
+const okfilePath = path.resolve('../product_data/OK_product.xlsx');
+const okworkbook = xlsx.readFile(okfilePath);
+
+// 選擇要讀取的工作表
+const oksheetName = okworkbook.SheetNames[0]; // 讀取第一個工作表
+const okworksheet = okworkbook.Sheets[oksheetName];
+
+// 將工作表轉換為 JSON 格式
+const okdata = xlsx.utils.sheet_to_json(okworksheet, { header: 1 });
+
+const okarray = okdata.slice(1).map(item => ({
+    "商店": item[0],
+    "品名": item[1],
+    "價格": item[2],
+    "熱量": item[3]
+  }));
+// 以上是ok的資料陣列------------------------------------------------------------------------------------------------------------------------------
+
+console.log('ok的資料陣列: ', okarray);
--- kcal_count_main/kcal_count2.js
+++ kcal_count_main/kcal_count2.js
@@ -1,4 +1,3 @@
-
 var ans = localStorage.getItem('outputtdee');
 document.getElementById('showtdee').textContent = "一天的建議攝取熱量: "+ans+" kcal";
     
